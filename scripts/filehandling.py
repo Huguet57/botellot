2079c1fc3087e8b5c853a3f2ba0b8b18a5ff6366
AHuguetUPC <andreu.huguet@estudiantat.upc.edu>
2021-12-10 02:07:18 +0800

    PDF parsing

--- scripts/filehandling.py
+++ scripts/filehandling.py
@@ -1,4 +1,7 @@
 from time import time
+from pdfparsing import fillPDFData
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
 RAW_FILES_PATH = '/home/andreu/botellot/files/'
 TICKETS_PATH = '/home/andreu/botellot/tickets/'
 
@@ -8,7 +11,7 @@ def downloadFile(update, context):
         context.bot.get_file(update.message.document).download(out=f)
     return timestamp
 
-def getPDFData(filename):
+def getTicketsData(filename):
     import os
     import qrtools
     from pdf2image import convert_from_path
@@ -17,35 +20,39 @@ def getPDFData(filename):
     fullpath = RAW_FILES_PATH + f"{filename}.pdf"
     tickets = convert_from_path(fullpath)
 
+    # Parse data from the PDF
     tickets_data = []
+    fillPDFData(tickets_data, fullpath)
 
     for i, ticket in enumerate(tickets):
-        # Step 0: Add new entry to tickets
-        tickets_data.append({})
-        data = tickets_data[i]
-
         # Step 1: Save with temporal name
         prepath = TICKETS_PATH + f"{filename}_{str(i)}.jpg"
         ticket.save(prepath, 'JPEG')
         
-        # Step 2: Get ticket data
-        # TODO
-
-        # Step 3: Decode QR and get its data
+        # Step 2: Decode QR and get its data
         qr.decode(filename=prepath)
-        data["qr"] = qr.data
+        tickets_data[i]["qr"] = qr.data
 
-        # Step 4: Change name to QR data
-        postpath = TICKETS_PATH + f"{data['qr']}.jpg"
+        # Step 3: Change name to QR data
+        postpath = TICKETS_PATH + f"{tickets_data[i]['qr']}.jpg"
         os.rename(prepath, postpath)
 
     return tickets_data
+
+def compose_msg(data):
+    return f"[{data['qr']}]\nEntrada pel {data['diasetmana']} ({data['date']})\nClub: {data['clubname']}\nPreu: {data['price']}€"
 
 def handlePDF(update, context):
     # Step 1: Download file
     filename = downloadFile(update, context)
 
     # Step 2: Get data from PDF
-    data = getPDFData(filename)
+    tickets_data = getTicketsData(filename)
 
-    print(data)
+    # Step 3: Reply with data
+    for ticket_data in tickets_data:
+        msg = compose_msg(ticket_data)
+
+        keyboard = [[InlineKeyboardButton("Demana-te-la", callback_data='0')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text(msg, reply_markup=reply_markup)
---
+++ scripts/pdfparsing.py
@@ -0,0 +1,91 @@
+def getPriceFromText(text):
+    price = 0.
+
+    for word in text:
+        if '€' in word:
+            potential_price = word.replace('€', '')
+            is_number = potential_price.replace('.','',1).isdigit()
+            if is_number: price = max(price, float(potential_price))
+
+    return price
+
+def getClubFromText(text):
+    isApolo = False
+    isRazz = False
+    
+    for word in text:
+        if 'Apolo' in word: isApolo = True
+        if 'Cupcake' in word: isApolo = True
+        if 'Nasty' in word: isApolo = True
+        if 'Cannibal' in word: isApolo = True
+
+        if 'Razz' in word: isRazz = True
+        if 'Dirty' in word: isRazz = True
+
+    if isApolo and isRazz: return 'Undefined'
+    if isApolo: return 'Apolo'
+    if isRazz: return 'Razzmatazz'
+
+    return 'Undefined'
+
+def isInRange(word, start, finish):
+    if not word.isdigit(): return False
+    return start <= int(word) and int(word) <= finish
+
+def getDateFromText(text):
+    for word in text:
+        if word.count('/') >= 2:
+            if len(word) < 10: continue
+
+            day = word[0:2]
+            month = word[3:5]
+            year = word[6:10]
+
+            isday = isInRange(day, 1, 31)
+            ismonth = isInRange(month, 1, 12)
+            isyear = isInRange(year, 2000, 2100)
+
+            if isday and ismonth and isyear:
+                return day + '/' + month + '/' + year
+
+    return 'Undefined'
+
+def getWeekdayFromText(day, month, year):
+    import datetime
+
+    weekday = datetime.datetime(
+        int(year),
+        int(month),
+        int(day)
+    ).weekday()
+
+    if weekday == 0: return 'Dilluns'
+    if weekday == 1: return 'Dimarts'
+    if weekday == 2: return 'Dimecres'
+    if weekday == 3: return 'Dijous'
+    if weekday == 4: return 'Divendres'
+    if weekday == 5: return 'Dissabte'
+    if weekday == 6: return 'Diumenge'
+
+    return 'Undefined'
+
+def fillPDFData(tickets_data, filename):
+    import fitz
+    doc = fitz.open(filename)
+
+    for i, page in enumerate(doc):
+        # Step 0: Initialize data
+        tickets_data.append({})
+        
+        # Step 1: Get text from page
+        text = page.get_text('text')
+        splitted_text = text.replace('\n',' ').replace('(',' ').replace(')',' ').split()
+        
+        # Step 2: Get data from page
+        tickets_data[i]['price'] = getPriceFromText(splitted_text)
+        tickets_data[i]['date'] = getDateFromText(splitted_text)
+        tickets_data[i]['clubname'] = getClubFromText(splitted_text)
+
+        if tickets_data[i]['date'] != 'Undefined':
+            day, month, year = tickets_data[i]['date'].split('/')
+            tickets_data[i]['diasetmana'] = getWeekdayFromText(day, month, year)
