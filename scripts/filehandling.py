f4fd1ac3752cbcff628a2f053e789761949607a4
AHuguetUPC <andreu.huguet@estudiantat.upc.edu>
2022-03-05 13:00:40 +0100

    Added parsing checks

--- scripts/filehandling.py
+++ scripts/filehandling.py
@@ -1,15 +1,20 @@
 from time import time
 from pdfparsing import fillPDFData
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import hashlib
 
 RAW_FILES_PATH = '/home/andreu/botellot/files/'
 TICKETS_PATH = '/home/andreu/botellot/tickets/'
 
 def downloadFile(update, context):
     timestamp = round(time()*1e6)
+    isPDF = update.message.document.mime_type == "application/pdf"
+
     with open(RAW_FILES_PATH + f"{timestamp}.pdf", 'wb') as f:
-        context.bot.get_file(update.message.document).download(out=f)
-    return timestamp
+        if isPDF:
+            context.bot.get_file(update.message.document).download(out=f)
+    
+    return isPDF, timestamp
 
 def getTicketsData(filename):
     import os
@@ -34,20 +39,32 @@ def getTicketsData(filename):
         tickets_data[i]["qr"] = qr.data
 
         # Step 3: Change name to QR data
-        postpath = TICKETS_PATH + f"{tickets_data[i]['qr']}.jpg"
-        os.rename(prepath, postpath)
+        if tickets_data[i]['clubname'] != 'Undefined':
+            postpath = TICKETS_PATH + f"{tickets_data[i]['qr']}.jpg"
+            os.rename(prepath, postpath)
+        else:
+            return {}
 
     return tickets_data
 
+def encode_name(name):
+    return hashlib.md5(name.encode()).hexdigest()
+
 def compose_msg(data):
-    return f"[{data['qr']}]\nEntrada pel {data['diasetmana']} ({data['date']})\nClub: {data['clubname']}\nPreu: {data['price']}€"
+    return f"[{encode_name(data['qr'])}]\nEntrada pel {data['diasetmana']} ({data['date']})\nClub: {data['clubname']}\nPreu: {data['price']}€"
 
 def handlePDF(update, context):
     # Step 1: Download file
-    filename = downloadFile(update, context)
+    correct, filename = downloadFile(update, context)
+    if not correct:
+        print("Not a PDF")
+        return
 
     # Step 2: Get data from PDF
     tickets_data = getTicketsData(filename)
+    if tickets_data == {}:
+        print("No ticket data found")
+        return
 
     # Step 3: Reply with data
     for ticket_data in tickets_data:
