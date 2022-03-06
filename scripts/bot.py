37b7c6a4b381698646765634599e524ff05dc31e
AHuguetUPC <andreu.huguet@estudiantat.upc.edu>
2022-03-06 23:15:15 +0100

    Send user a private message as callback

--- scripts/bot.py
+++ scripts/bot.py
@@ -11,7 +11,7 @@ updater = Updater(BOT_TOKEN, use_context=True)
 updater.dispatcher.add_handler(CommandHandler('start', commands.start))
 updater.dispatcher.add_handler(CommandHandler('publicar', commands.publish))
 updater.dispatcher.add_handler(MessageHandler(Filters.document, filehandling.handlePDF))
-updater.dispatcher.add_handler(CallbackQueryHandler(callback.user))
+updater.dispatcher.add_handler(CallbackQueryHandler(callback.private_text))
 
 updater.start_polling()
 updater.idle()
--- scripts/callback.py
+++ scripts/callback.py
@@ -10,3 +10,19 @@ def user(update: Update, context: CallbackContext) -> None:
     query.answer()
 
     query.edit_message_text(text=f"@{query.from_user.username} ha seleccionat l'opció.")
+
+def private_text(update: Update, context: CallbackContext) -> None:
+    """Sends a private text to user that has responded"""
+    query = update.callback_query
+    query.answer()
+
+    # Editar missatge públic
+    keyboard = [[InlineKeyboardButton("Agafades.", callback_data='3')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(text=f"Missatge enviat a @{query.from_user.username}.")
+    query.edit_message_reply_markup(reply_markup=reply_markup)
+
+    # Enviar missatge en privat
+    keyboard = [[InlineKeyboardButton("Comprar", callback_data='3')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.from_user.send_message("Missatge enviat privadament a l'usuari.", reply_markup=reply_markup)
