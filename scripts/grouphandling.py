45ed9ccb62b34995d6a1d705f2d64677055284f1
AHuguetUPC <andreu.huguet@estudiantat.upc.edu>
2022-03-08 13:54:27 +0100

    JSON constants in uppercase

--- scripts/grouphandling.py
+++ scripts/grouphandling.py
@@ -10,8 +10,8 @@ config = json.load(open("./config.json"))
 # TODO: Encode URLs
 def send_public_msg(msg):
     
-    bot_token = config["bot_token"]
-    group_chatID = config["group_chatID"]
+    bot_token = config["BOT_TOKEN"]
+    group_chatID = config["GROUP_CHAT_ID"]
     sample_markdown_message = '%2Abold%20text%2A%0A_italic%20text_%0A%5Binline%20URL%5D%28http%3A%2F%2Fwww.example.com%2F%29%0A%5Binline%20mention%20of%20a%20user%5D%28tg%3A%2F%2Fuser%3Fid%3D123456789%29%0A%60inline%20fixed-width%20code%60%0A%60%60%60%0Apre-formatted%20fixed-width%20code%20block%0A%60%60%60%0A%60%60%60python%0Apre-formatted%20fixed-width%20code%20block%20written%20in%20the%20Python%20programming%20language%0A%60%60%60'
     
     # TODO: Crear un fitxer de configuració amb textos públics
